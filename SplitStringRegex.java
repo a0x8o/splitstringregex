738a3c2be9da72eb9439bc4d54091183a18d6c2b
Alex B <axbaretto@gmail.com>
2017-05-30 07:24:44 -0700

    Cleaned up some formatting

--- SplitStringRegex.java
+++ SplitStringRegex.java
@@ -14,38 +14,38 @@ import java.util.regex.*;
 
 public class SplitStringRegex {
 
-		public SplitStringRegex() {
-		}
+    public SplitStringRegex() {
+    }
 
-		public static void main(String args[]) {
-		    System.out.println("SplitString - Regular Expression Solution");
-            System.out.println("=========================================");
-            String input = "\"Hello World\" marker coffee \"Headphones eraser\"";
-		    System.out.println("Return: " + splitStringToArray(input));
-            String input1 = "!cmd 45 90 \"An argument\" Another AndAnother \"Another one in quotes\"";
-                System.out.println("Return: " + splitStringToArray(input1).toString());
-                String input2 = "'foo bar' c;\"baz,blurb\" d;\"quux,syzygy\"";
-                System.out.println("Return: " + splitStringToArray(input2).toString());
-                String input3 = "";
-                System.out.println("Return: " + splitStringToArray(input3).toString());
-                if (args != null) {
-                    for (String s : args) {
-                        System.out.println("Return: " + splitStringToArray(s).toString());
-                    }
-                }
-		}
+    public static void main(String args[]) {
+        System.out.println("SplitString - Regular Expression Solution");
+        System.out.println("=========================================");
+        String input = "\"Hello World\" marker coffee \"Headphones eraser\"";
+        System.out.println("Return: " + splitStringToArray(input));
+        String input1 = "!cmd 45 90 \"An argument\" Another AndAnother \"Another one in quotes\"";
+        System.out.println("Return: " + splitStringToArray(input1).toString());
+        String input2 = "'foo bar' c;\"baz,blurb\" d;\"quux,syzygy\"";
+        System.out.println("Return: " + splitStringToArray(input2).toString());
+        String input3 = "";
+        System.out.println("Return: " + splitStringToArray(input3).toString());
+        if (args != null) {
+            for (String s : args) {
+                System.out.println("Return: " + splitStringToArray(s).toString());
+            }
+        }
+    }
 
-		static List<String> splitStringToArray(String input) {
-		    String splitConditions = "([^\"]\\S*|\".+?\")\\s*";
-		    // [^"]  -- token starting with something other than "
-		    // \S*   -- followed by zero of more non-space characters
-		    // |     -- or
-		    // ".+?" -- a " symbol followed by non " character(s), until another "
-		    List<String> list = new ArrayList<String>();
-		    Matcher m = Pattern.compile(splitConditions).matcher(input);
-		    while (m.find())
-			list.add(m.group(1));
-
-		    return list;
-		}
+    static List<String> splitStringToArray(String input) {
+        String splitConditions = "([^\"]\\S*|\".+?\")\\s*";
+	// [^"]  -- token starting with something other than "
+	// \S*   -- followed by zero of more non-space characters
+	// |     -- or
+	// ".+?" -- a " symbol followed by non " character(s), until another "
+	List<String> list = new ArrayList<String>();
+	Matcher m = Pattern.compile(splitConditions).matcher(input);
+	while (m.find())
+	    list.add(m.group(1));
+        
+	return list;
+    }
 }
